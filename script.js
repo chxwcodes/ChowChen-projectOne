9f3c4045deae549aed7a91647251772b00757aed
Chow <contact-chxw@gmail.com>
2022-07-08 15:11:01 -0400

    added gallery js feature

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-//global variables
-const slideOutNav = document.querySelector('.mobileNav div');
-
-///////////////////////////////////////////////////////////////////////////
 //SLIDE OUT MOBILE MENU:
+const slideOutNav = document.querySelector('.mobileNav div');
 
 //when the hamburger menu is clicked, display the slideout menu:
 const mobileBtn = document.querySelector('.mobileNav i');
@@ -18,4 +15,36 @@ closeBtn.addEventListener('click', function() {
     slideOutNav.style.top = '-500px';
 })
 
+//IMAGE GALLERY SLIDESHOW:
+const slides = document.querySelectorAll('.banner li');
+
+const btnNext = document.querySelector('.btnNext');
+const btnPrev = document.querySelector('.btnPrev');
+
+let currentSlide = 0;
+const maxSlide = slides.length - 1;
+
+btnNext.addEventListener('click', function() {
+    if (currentSlide === maxSlide) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    });
+});
+
+btnPrev.addEventListener('click', function () {
+    if (currentSlide === 0) {
+        currentSlide = maxSlide;
+    } else {
+        currentSlide--;
+    }
+
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    });
+});
 
